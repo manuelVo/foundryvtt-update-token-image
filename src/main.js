756e513305e2571251597058f3d0716941499e85
Manuel Vögele <develop@manuel-voegele.de>
2021-11-04 11:58:04 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,49 @@
+import { libWrapper } from "./libwrapper_shim.js";
+
+Hooks.once("init", () => {
+	libWrapper.register("update-token-image", "FilePicker.prototype._renderInner", renderInner, "WRAPPER");
+	libWrapper.register("update-token-image", "ActorSheet.prototype._onEditImage", onEditImage, "OVERRIDE");
+	libWrapper.register("update-token-image", "ActorSheet.prototype._updateObject", updateObject, "WRAPPER");
+});
+
+
+async function renderInner(wrapped, data) {
+	const options = this.options;
+	const html = await wrapped(data);
+	if (options.updateTokenImageEnable) {
+		html.find("footer > button").before('<div class="form-group"><input type="checkbox" id="update-token-image" name="update-token-image"/><label for="update-token-image">Update Token Image</label></div>');
+		html.find("#update-token-image").prop("checked", options.updateTokenImageChecked);
+		html.find("#update-token-image").change(function () {options.updateTokenImageChecked = $(this).prop("checked");});
+	}
+	return html;
+}
+
+
+function onEditImage(event) {
+	const attr = event.currentTarget.dataset.edit;
+	const current = getProperty(this.actor.data, attr);
+	const updateTokenImageChecked = !this.object.data.token.randomImg && this.object.data.img === this.object.data.token.img;
+	const fp = new FilePicker({
+		type: "image",
+		current: current,
+		callback: (path, picker) => {
+			event.currentTarget.src = path;
+			event.updateTokenImageChecked = picker.options.updateTokenImageChecked;
+			this._onSubmit(event);
+		},
+		top: this.position.top + 40,
+		left: this.position.left + 10,
+		updateTokenImageEnable: true,
+		updateTokenImageChecked: updateTokenImageChecked,
+	});
+	return fp.browse();
+}
+
+async function updateObject(wrapped, event, formData) {
+	if ( !this.object.id )
+		return;
+	const result = await wrapped(event, formData);
+	if (event.updateTokenImageChecked)
+		await this.object.data.token.update({img: formData.img});
+	return result;
+}
