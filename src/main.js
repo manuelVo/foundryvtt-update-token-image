aeac749c77933032fd0ebca54cb320239a36a0c3
Manuel Vögele <develop@manuel-voegele.de>
2022-01-11 15:37:37 +0100

    Add translation support

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ async function renderInner(wrapped, data) {
 	const options = this.options;
 	const html = await wrapped(data);
 	if (options.updateTokenImageEnable) {
-		html.find("footer > button").before('<div class="form-group"><input type="checkbox" id="update-token-image" name="update-token-image"/><label for="update-token-image">Update Token Image</label></div>');
+		html.find("footer > button").before(`<div class="form-group"><input type="checkbox" id="update-token-image" name="update-token-image"/><label for="update-token-image">${game.i18n.localize("update-token-image.updateTokenImage")}</label></div>`);
 		html.find("#update-token-image").prop("checked", options.updateTokenImageChecked);
 		html.find("#update-token-image").change(function () {options.updateTokenImageChecked = $(this).prop("checked");});
 	}
